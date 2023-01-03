f8357d8b34c0880a64d9ae5f97481e382d8366f2
Lucas Bombarda <lucasbombardati@gmail.com>
2023-01-03 13:52:04 -0300

    started search customer

---
+++ core/search/search_customer.py
@@ -0,0 +1,11 @@
+from database.database import Database
+
+class SearchCustomer:
+    def __init__(self, user) -> None:
+        self._user = user
+
+    def search(self) -> list:
+        return []
+    
+    def get_search_info(self) -> dict:
+        return {}
